feb62a950861588cd9ed8a85b7c6367a23f0df66
Nicolas Hallaert <nicolas.hallaert.pro@gmail.com>
2021-09-22 01:04:29 +0200

    chore: useless files

--- dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.customVars = void 0;
-exports.customVars = {
-    loglevel: process.env.NODE_ENV,
-    jwt_secret: process.env.jwt_secret
-};
