a4b1aeb8795018a11a31e7a7fea673c7a495d7a1
Nicolas Hallaert <nicolas.hallaert.pro@gmail.com>
2021-09-22 01:01:49 +0200

    refactor(health): more inf

--- src/routes/v1/health/index.ts
+++ src/routes/v1/health/index.ts
@@ -5,6 +5,9 @@ import { FastifyInstance } from "fastify/types/instance";
 export async function health(server: FastifyInstance) {
   server.get("/health", async function health() {
     return {
+      name: "Random Service",
+      description: "Service description",
+      version: "1.0.0",
       uptime: process.uptime()
     }
   });
